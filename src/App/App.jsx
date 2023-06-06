981a7eb474ab339d010cf8a413fcf043f4d19dcf
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-06-06 21:20:53 +0300

    add react icons

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,9 +1,19 @@
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-    
+      <div className="container">
+        <div id="break-label">
+          <p>Break Length</p>
+          <button></button>
+          <p>5</p>
+          <button></button>
+        </div>
+        <div id="session-label">
+          <p>Session Length</p>
+        </div>
+      </div>
     </div>
   );
 }
