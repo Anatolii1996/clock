2912a15d0900b96b1b01383615411d429d026074
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-06-09 20:17:37 +0300

    styled to session length

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,18 +1,25 @@
+import { useState } from "react";
 import "./App.scss";
 import { FaArrowDown, FaArrowUp } from "react-icons/fa";
 
 function App() {
+  const [breakCount, setBreakCount] = useState(5);
+  const [sessionCount, setSessionCount] = useState(25);
+
   return (
     <div className="App">
       <div className="container">
         <div id="break-label">
           <p className="label">Break Length</p>
           <button><FaArrowDown /> </button>
-          <p className="count">5</p>
+          <p className="count">{breakCount}</p>
           <button><FaArrowUp/></button>
         </div>
         <div id="session-label">
-          <p>Session Length</p>
+          <p className="label">Session Length</p>
+          <button><FaArrowDown /> </button>
+          <p className="count">{sessionCount}</p>
+          <button><FaArrowUp/></button>
         </div>
       </div>
     </div>
