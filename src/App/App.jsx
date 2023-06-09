0b0f002de3371887d4bed50137b1ca4f43fe095b
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-06-09 20:32:34 +0300

    add function incr/decr

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.scss";
 import { FaArrowDown, FaArrowUp } from "react-icons/fa";
 
@@ -6,20 +6,42 @@ function App() {
   const [breakCount, setBreakCount] = useState(5);
   const [sessionCount, setSessionCount] = useState(25);
 
+  const increment = (arg) => {
+    arg((prev) => prev + 1);
+  };
+
+  const decrement = (arg) => {
+    arg((prev) => {
+      if (prev > 1) {
+        return prev - 1;
+      } else {
+        return prev;
+      }
+    });
+  };
+
   return (
     <div className="App">
       <div className="container">
         <div id="break-label">
           <p className="label">Break Length</p>
-          <button><FaArrowDown /> </button>
+          <button onClick={()=>decrement(setBreakCount)}>
+            <FaArrowDown />{" "}
+          </button>
           <p className="count">{breakCount}</p>
-          <button><FaArrowUp/></button>
+          <button onClick={() => increment(setBreakCount)}>
+            <FaArrowUp />
+          </button>
         </div>
         <div id="session-label">
           <p className="label">Session Length</p>
-          <button><FaArrowDown /> </button>
+          <button onClick={()=>decrement(setSessionCount)}>
+            <FaArrowDown />{" "}
+          </button>
           <p className="count">{sessionCount}</p>
-          <button><FaArrowUp/></button>
+          <button onClick={() => increment(setSessionCount)}>
+            <FaArrowUp />
+          </button>
         </div>
       </div>
     </div>
